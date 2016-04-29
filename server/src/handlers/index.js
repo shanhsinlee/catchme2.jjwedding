3919f3738a7d2dee6896b19bfd491175ce2a1919
kerkerj <jerryhuang@kkbox.com>
2016-04-30 03:12:58 +0800

    fix: remove test handler

--- server/src/handlers/index.js
+++ server/src/handlers/index.js
@@ -1,5 +1,4 @@
 exports.index = require("./index_handler.js")
-exports.test = require("./test_handler.js")
 exports.login = require("./login_handler.js")
 exports.submit = require("./submit_handler.js")
 exports.score = require("./score_handler.js")
