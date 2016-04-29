e8d273444013a6ea9306a678ab61059b7c02a1b0
kerkerj <jerryhuang@kkbox.com>
2016-04-30 02:52:15 +0800

    feat: add check uid middleware

--- server/src/handlers/submit_handler.js
+++ server/src/handlers/submit_handler.js
@@ -1,5 +1,7 @@
 import redis from '../utils/database.js'
 
 module.exports = (req, res) => {
+  let uid = req.params.uid
+  
   res.send("submit")
 }
--- server/src/main.js
+++ server/src/main.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
+import redis from './utils/database.js'
 import handlers from './handlers'
 
 const PORT = 5566
@@ -16,12 +17,25 @@ app.use(bodyParser.urlencoded({
   extended: true
 }))
 
+let isUidValid = (req, res, next) => {
+  // check uid
+  redis.exists(`user:${req.params.uid}`, (err, exists) => {
+    if (exists) {
+      return next()
+    }
+    else {
+      // failed
+      return res.json({ code: 0, msg: "失敗" })
+    }
+  })
+}
+
 // routes
 app.get('/', handlers.index)
 app.get('/test', handlers.test)
 app.post('/login', handlers.login)
-app.post('/user/:uid/submit', handlers.submit)
-app.get('/user/:uid/score', handlers.score)
+app.post('/user/:uid/submit', isUidValid, handlers.submit)
+app.get('/user/:uid/score', isUidValid, handlers.score)
 app.get('/user/:uid', handlers.user)
 
 app.listen(PORT, () => {
