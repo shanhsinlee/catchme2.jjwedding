d37cdeb88a07376e4b86c2c1f025edc6d7f734dd
kerkerj <jerryhuang@kkbox.com>
2016-05-01 13:42:39 +0800

    feat: change to append submit value

--- src/handlers/submit_handler.js
+++ src/handlers/submit_handler.js
@@ -2,7 +2,7 @@ import redis from '../utils/database.js'
 
 module.exports = (req, res) => {
   let action = req.body.action
-  let value = req.body.value || 0 // TODO ask this value is for appending or it's a total number
+  let value = req.body.value || 0
   let updateKey = ""
 
   if (action === "hit") {
@@ -16,12 +16,34 @@ module.exports = (req, res) => {
     return res.status(400).json({ msg: "失敗 (未提供 action params)" })
   }
 
-  redis.hmset(`user:${req.params.uid}`, updateKey, value, (err) => {
-    if (err) {
-      return res.status(400).json({ msg: "失敗 (更新失敗)" })
-    }
-    else {
-      return res.status(200).json({ msg: "成功" })
-    }
+  let getUserScore = new Promise((resolve, reject) => {
+    redis.hget(`user:${req.params.uid}`, updateKey, (err, score) => {
+      if (err) {
+        reject("失敗 (查詢分數失敗)")
+      }
+      else {
+        resolve(score)
+      }
+    })
+  })
+
+  getUserScore.then((score) => {
+    return new Promise((resolve, reject) => {
+      let scoreToWrite = (+score) + (+value)
+      redis.hmset(`user:${req.params.uid}`, updateKey, scoreToWrite, (err) => {
+        if (err) {
+          reject("失敗 (更新失敗)")
+        }
+        else {
+          resolve(scoreToWrite)
+        }
+      })
+    })
+  })
+  .then((score) => {
+    return res.status(200).json({ msg: "成功", action: updateKey, score: score })
+  })
+  .catch((reason) => {
+    res.status(400).json({ msg: msg })
   })
 }
